f6152afa374a16ac2286b565fdf0003dabcddcf8
d4oq <88bt3ncs53n@gmail.com>
2021-07-20 22:17:20 +0900

    add global style

---
+++ src/components/GlobalStyle.tsx
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 1rem;
+  }
+`
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom'
 import { Reset } from 'styled-reset'
+import { GlobalStyle } from './components/GlobalStyle'
 import { App } from './components/App'
 
 ReactDOM.render(
   <>
     <Reset />
+    <GlobalStyle />
     <App />
   </>,
   document.getElementById('app')
